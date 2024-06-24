6b658901df3d4f81bf8f9b80b87aee4cf273e77b
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-24 13:57:29 +0530

    Add new code generated at 2024-06-24T13:57:26.938107

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        UtilityBillManager utilityBillManager = new UtilityBillManager();
+        Computer computer = new Computer("Dell XPS", "Dell", 1000);
+        computer.printDetails();
 
-        // Add some utility bills
-        utilityBillManager.addUtilityBill(new UtilityBill(1, "Electric", 100.00, true));
-        utilityBillManager.addUtilityBill(new UtilityBill(2, "Water", 50.00, false));
-        utilityBillManager.addUtilityBill(new UtilityBill(3, "Gas", 75.00, true));
-
-        // Get the utility bill summary
-        String summary = utilityBillManager.getUtilityBillSummary();
-
-        // Print the summary
-        System.out.println(summary);
+        Laptop laptop = new Laptop("MacBook Air", "Apple", 1200, 10);
+        laptop.printDetails();
     }
 }
