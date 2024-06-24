70b223a0c5031b452e89bc25fc82905813fd9bd3
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-24 15:52:28 +0530

    Add new code generated at 2024-06-24T15:52:27.306809

--- UtilityBillManagement.java
+++ UtilityBillManagement.java
@@ -1,51 +1,120 @@
 public class UtilityBillManagement {
 
-    private List<Bill> bills;
-    private List<Payment> payments;
-
-    public UtilityBillManagement(List<Bill> bills, List<Payment> payments) {
-        this.bills = bills;
-        this.payments = payments;
-    }
-
-    public Map<String, Double> getBillSummary() {
-        Map<String, Double> summary = new HashMap<>();
-        for (Bill bill : bills) {
-            summary.put(bill.getUtilityType(), bill.getAmount());
-        }
-        return summary;
-    }
-
-    public Map<String, Double> getPaymentSummary() {
-        Map<String, Double> summary = new HashMap<>();
-        for (Payment payment : payments) {
-            summary.put(payment.getUtilityType(), payment.getAmount());
-        }
-        return summary;
-    }
-
     public static void main(String[] args) {
-        List<Bill> bills = new ArrayList<>();
-        bills.add(new Bill("Electricity", 100.0));
-        bills.add(new Bill("Water", 50.0));
-        bills.add(new Bill("Gas", 75.0));
+        // Create a list of utility bills
+        List<UtilityBill> utilityBills = List.of(
+                new UtilityBill("Electricity", 100.0, "2023-03-15"),
+                new UtilityBill("Gas", 50.0, "2023-04-15"),
+                new UtilityBill("Water", 25.0, "2023-05-15")
+        );
 
-        List<Payment> payments = new ArrayList<>();
-        payments.add(new Payment("Electricity", 100.0));
-        payments.add(new Payment("Water", 50.0));
+        // Create a list of utility payments
+        List<UtilityPayment> utilityPayments = List.of(
+                new UtilityPayment("Electricity", 100.0, "2023-03-15"),
+                new UtilityPayment("Gas", 50.0, "2023-04-15")
+        );
 
-        UtilityBillManagement utilityBillManagement = new UtilityBillManagement(bills, payments);
-
-        Map<String, Double> billSummary = utilityBillManagement.getBillSummary();
-        System.out.println("Bill Summary:");
-        for (Map.Entry<String, Double> entry : billSummary.entrySet()) {
-            System.out.println(entry.getKey() + ": $" + entry.getValue());
+        // Display a summary of all utility bills and payments
+        System.out.println("Utility Bill Summary");
+        System.out.println("--------------------");
+        for (UtilityBill bill : utilityBills) {
+            System.out.println(bill);
+        }
+        for (UtilityPayment payment : utilityPayments) {
+            System.out.println(payment);
         }
 
-        Map<String, Double> paymentSummary = utilityBillManagement.getPaymentSummary();
-        System.out.println("Payment Summary:");
-        for (Map.Entry<String, Double> entry : paymentSummary.entrySet()) {
-            System.out.println(entry.getKey() + ": $" + entry.getValue());
+        // Display detailed information on individual utility transactions
+        System.out.println("Detailed Transaction Information");
+        System.out.println("----------------------------");
+        for (UtilityTransaction transaction : utilityBills) {
+            System.out.println(transaction);
         }
+        for (UtilityTransaction transaction : utilityPayments) {
+            System.out.println(transaction);
+        }
+    }
+
+    public static class UtilityBill implements UtilityTransaction {
+
+        private String type;
+        private double amount;
+        private String dueDate;
+
+        public UtilityBill(String type, double amount, String dueDate) {
+            this.type = type;
+            this.amount = amount;
+            this.dueDate = dueDate;
+        }
+
+        @Override
+        public String getType() {
+            return type;
+        }
+
+        @Override
+        public double getAmount() {
+            return amount;
+        }
+
+        @Override
+        public String getDueDate() {
+            return dueDate;
+        }
+
+        @Override
+        public String toString() {
+            return "UtilityBill{" +
+                    "type='" + type + '\'' +
+                    ", amount=" + amount +
+                    ", dueDate='" + dueDate + '\'' +
+                    '}';
+        }
+    }
+
+    public static class UtilityPayment implements UtilityTransaction {
+
+        private String type;
+        private double amount;
+        private String date;
+
+        public UtilityPayment(String type, double amount, String date) {
+            this.type = type;
+            this.amount = amount;
+            this.date = date;
+        }
+
+        @Override
+        public String getType() {
+            return type;
+        }
+
+        @Override
+        public double getAmount() {
+            return amount;
+        }
+
+        @Override
+        public String getDueDate() {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return "UtilityPayment{" +
+                    "type='" + type + '\'' +
+                    ", amount=" + amount +
+                    ", date='" + date + '\'' +
+                    '}';
+        }
+    }
+
+    public interface UtilityTransaction {
+
+        String getType();
+
+        double getAmount();
+
+        String getDueDate();
     }
 }
