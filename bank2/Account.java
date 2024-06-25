419c6b1490e92ec9c1f22678d025fa16199bb780
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-25 14:24:51 +0530

    Add new code generated at 2024-06-25T14:24:48.229371

---
+++ bank2/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    // Getters and setters
+    // ...
+
+}
