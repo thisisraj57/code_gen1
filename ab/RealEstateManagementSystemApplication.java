4a31f6b922c050621d5ca1fe3ca3cc4edd04d640
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-25 17:32:28 +0530

    Add new code generated at 2024-06-25T17:32:27.242441

---
+++ ab/RealEstateManagementSystemApplication.java
@@ -0,0 +1,5 @@
+public class RealEstateManagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateManagementSystemApplication.class, args);
+    }
+}
