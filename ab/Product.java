dbf6e570208131509b7f04c8cc746e06785fc42a
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 01:47:49 +0530

    Add new code generated at 2024-06-26T01:47:47.193112

---
+++ ab/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+    // Getters and setters
+}
