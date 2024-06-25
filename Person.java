b07c7eb03b468edda7ce0be588eb2cbf0fce64a8
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-25 10:42:07 +0530

    Add new code generated at 2024-06-25T10:42:05.485818

---
+++ Person.java
@@ -0,0 +1,22 @@
+public class Person {
+
+    private String name;
+    private String city;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
