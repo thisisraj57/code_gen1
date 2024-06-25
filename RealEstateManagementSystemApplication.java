53bb93db92dbe4c80f4617a78936c11b317115a7
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 01:30:26 +0530

    Add new code generated at 2024-06-26T01:30:24.036113

---
+++ RealEstateManagementSystemApplication.java
@@ -0,0 +1,7 @@
+public class RealEstateManagementSystemApplication {
+
+    // The main method, where the Spring application starts
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateManagementSystemApplication.class, args);
+    }
+}
