a45e7634d4deed782f95a9ca82d1d0e94a4529ce
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 09:25:31 +0530

    Add new code generated at 2024-06-26T09:25:28.639641

---
+++ AccountController.java
@@ -0,0 +1,30 @@
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    @GetMapping("/{id}")
+    public Account getAccount(@PathVariable Long id) {
+        return accountService.getAccount(id);
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @PutMapping("/{id}")
+    public Account updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        return accountService.updateAccount(id, account);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+    }
+}
