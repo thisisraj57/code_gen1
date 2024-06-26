5bf92a33e7c85bf1941842b563a42fe55615c6ef
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 09:43:27 +0530

    Add new code generated at 2024-06-26T09:43:25.226992

---
+++ UniversityManagementSystemApplication.java
@@ -0,0 +1,6 @@
+public class UniversityManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityManagementSystemApplication.class, args);
+    }
+}
