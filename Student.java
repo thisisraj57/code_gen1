3936f0e931868b80db9ecabae5715f6115893575
KANISHKA RAJ <87484461+thisisraj57@users.noreply.github.com>
2024-06-26 09:43:11 +0530

    Add new code generated at 2024-06-26T09:43:09.475263

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    // Getters and Setters
+}
